05880cf4e4e8917526384c901e10184545645298
Polar <tpwns73@gmail.com>
2023-08-11 13:28:58 +0900

    Tap Screen, then hide key board

--- sora/lib/screens/Login/login.dart
+++ sora/lib/screens/Login/login.dart
@@ -6,6 +6,7 @@ import 'package:sora/screens/Home/utils/bottom_navigate.dart';
 import 'package:provider/provider.dart';
 import 'package:sora/providers/login_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:sora/screens/Regist/regist.dart';
 import 'package:sora/utils/urls.dart';
 
 class LoginPage extends StatefulWidget {
@@ -53,66 +54,83 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     Login loginInfo = Provider.of<Login>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
-              TextField(
-                controller: idController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'id',
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child : Scaffold(
+        appBar: AppBar(
+          title: const Text('Login'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                TextField(
+                  controller: idController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'id',
+                  ),
                 ),
-              ),
-              TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'password',
+                const SizedBox(height: 10,),
+                TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'password',
+                  ),
                 ),
-              ),
-              FilledButton(
-                onPressed: () async {
-                  await storage.write(
-                    key: 'login',
-                    value: "id${idController.text} password${passwordController.text}",
-                  );
-                  var result = await http.post(
-                    Uri.parse(loginUrl.toString()),
-                    headers: {
-                      "Accept": "application/json",
-                      "Content-Type": "application/json",
-                    },
-                    body: json.encode({
-                      "email": idController.text,
-                      "user_pw": passwordController.text,
-                    }),
-                  );
-                  if (result.statusCode == 200) {
-                    print(result.body);
-                    loginInfo.setEmail(idController.text);
-                    Navigator.pushReplacement(context, 
+                const SizedBox(height: 10,),
+                FilledButton(
+                  onPressed: () async {
+                    await storage.write(
+                      key: 'login',
+                      value: "id${idController.text} password${passwordController.text}",
+                    );
+                    var result = await http.post(
+                      Uri.parse(loginUrl.toString()),
+                      headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json",
+                      },
+                      body: json.encode({
+                        "email": idController.text,
+                        "user_pw": passwordController.text,
+                      }),
+                    );
+                    if (result.statusCode == 200) {
+                      print(result.body);
+                      loginInfo.setEmail(idController.text);
+                      Navigator.pushReplacement(context, 
+                        MaterialPageRoute(
+                          builder: (context) => const HomeNavigationBar(),
+                        ),
+                      );
+                    } else {
+                      print(result.statusCode);
+                      _showDialog('Failed to login');
+                    }
+                  },
+                  child: const Text('Login'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
                       MaterialPageRoute(
-                        builder: (context) => const HomeNavigationBar(),
+                        builder: (context) => const RegistPage(),
                       ),
                     );
-                  } else {
-                    print(result.statusCode);
-                    _showDialog('Failed to login');
-                  }
-                },
-                child: const Text('Login'),
-              )
-            ],
-          )
-        ),
-      )
+                  },
+                  child: const Text('Sign up'),
+                ),
+              ],
+            )
+          ),
+        )
+      ),
     );
   }
   void _showDialog(String message) {
--- sora/lib/screens/Regist/regist.dart
+++ sora/lib/screens/Regist/regist.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:sora/utils/urls.dart';
 
 class RegistPage extends StatefulWidget {
   const RegistPage({Key? key}) : super(key: key);
@@ -10,16 +14,130 @@ class RegistPage extends StatefulWidget {
 
 class _RegistPageState extends State<RegistPage> {
   // This is the state of the regist page.
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  late TextEditingController passwordConfirmController;
+  late TextEditingController genderController;
+  late TextEditingController phoneController;
+  late TextEditingController universityController;
+  late TextEditingController departmentController;
+  late TextEditingController studentIdController;
+  late TextEditingController descriptionController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: BackButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          color: ColorScheme.fromSwatch(primarySwatch: Colors.lightGreen)
+              .secondary,
+        ),
         title: const Text('Regist'),
       ),
-      body: const Center(
-        child: Text(
-          'Regist Page',
-          style: TextStyle(fontSize: 36),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'id',
+                ),
+              ),
+              const SizedBox(height: 10), 
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'password',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: passwordConfirmController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'password confirm',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: genderController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'gender',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: phoneController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'phone',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: universityController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'university',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: departmentController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'department',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: studentIdController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'student id',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'description',
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () async {
+                  var reslut = await http.post(
+                    Uri.parse(registUrl.toString()),
+                    headers: {
+                      "Accept": "application/json",
+                      "Content-Type": "application/json",
+                    },
+                    body: json.encode({
+                      "email": emailController.text,
+                      "user_pw": passwordController.text,
+                      "phone": phoneController.text,
+                      "university": universityController.text,
+                      "department": departmentController.text,
+                      "student_id": studentIdController.text,
+                      "description": descriptionController.text,
+                    })
+                  );
+                },
+                child: const Text('Regist'),
+              ),
+            ],
+          )
         ),
       ),
     );
